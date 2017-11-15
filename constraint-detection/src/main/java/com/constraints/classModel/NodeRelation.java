3bd876713b6483ff053934fb2cd4985ff05d747d
Andrés Vazquez <andavazgar@users.noreply.github.com>
2017-11-15 18:17:35 -0500

    Created  module
    Created module in order to avoid circular dependency.

---
+++ constraint-detection/src/main/java/com/constraints/classModel/NodeRelation.java
@@ -0,0 +1,36 @@
+package com.constraints.classModel;
+
+import com.horstmann.violet.product.diagram.abstracts.node.INode;
+
+// An inner class Relation is declared so that AbstractNode and AbstractGraph can
+// use it to implement the constrain detection
+public class NodeRelation 
+{
+	private String relation = null;
+	private INode node = null;
+	
+	public NodeRelation()
+	{}
+	
+	public NodeRelation(String relation, INode node)
+	{
+		this.relation = relation;
+		this.node = node;
+	}
+
+	public INode getNode()
+	{
+		return this.node;
+	}
+	
+	public String getRelation()
+	{
+		return this.relation;
+	}
+	
+	public void showRelationInfo()
+	{
+		System.out.println("Relationship: " + relation);
+		System.out.println("Node : " + node);
+	}
+}
